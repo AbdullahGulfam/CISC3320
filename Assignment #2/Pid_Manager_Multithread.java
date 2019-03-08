a12412f328fb9330b4957d749c4ccf03d88963ea
Abdullah Gulfam <abdullahgulfam1997@gmail.com>
2019-03-08 06:06:15 -0500

    Documentation
    - Added inline comments and documentation to Pid_Manager_Thread and Pid_Manager_Multithread

--- Assignment #2/Pid_Manager_Multithread.java
+++ Assignment #2/Pid_Manager_Multithread.java
@@ -1,20 +1,40 @@
+/*
+*   Create a multithreaded program that tests the program originally created in Assignment #1.    
+*   Create several threads (see Pid_Manager_Multithread), make them each request a pid, put them each to sleep for a random duration of time, and finally release the pid.
+*   
+*   Author(s):  Abdullah Gulfam and Faris Kurtagic 
+*                Abdullah was responsible for implementation of the Pid_Manager_Thread file and documentation.
+*                Faris was responsible for implementation of the Pid_Manager_Multithread file.
+*   
+*   Assumptions:    Although the project called for a random duration of sleep for the threads, we assumed it would make sense to have some range/limit for testing purposes to ensure all threads were allocated a pid, put to sleep, and had their pid released within a reasonable time frame
+*/
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.Random;
 
 public class Pid_Manager_Multithread {
     public static void main(String[] args) {
+        //  Create a Pid_Manager to pass into Pid_Manager_Thread constructor
         Pid_Manager pidManagerMain = new Pid_Manager();
+
+        //  Allocate the map for the Pid_Manager
         pidManagerMain.allocate_map();
+
+        //  Create a random number generation to determine the duration of sleep for a thread
         Random randomSleepTime = new Random();
 
+        //  Create a ExecutorService pool to manage 100 threads  
         ExecutorService pidPool = Executors.newFixedThreadPool(100);
 
+        //  Loop through pool, create a thread by passing a unique id, a random duration of sleep between 1000 and 10000 milliseconds, and a Pid_Manager 
+        //  Execute each thread in the pool
         for (int i = 0; i < 100; i++) {
             Pid_Manager_Thread pidThread = new Pid_Manager_Thread(i, randomSleepTime.nextInt(10000) + 1000, pidManagerMain);
             pidPool.execute(pidThread);
         }
         
+        //  Shut the ExecutorService down after executing 100 threads
         pidPool.shutdown();
     }
 }
--- Assignment #2/Pid_Manager_Thread.java
+++ Assignment #2/Pid_Manager_Thread.java
@@ -1,8 +1,19 @@
+/*
+*   Create a multithreaded program that tests the program originally created in Assignment #1.    
+*   Create several threads (see Pid_Manager_Multithread), make them each request a pid, put them each to sleep for a random duration of time, and finally release the pid.
+*   
+*   Author(s):  Abdullah Gulfam and Faris Kurtagic 
+*                Abdullah was responsible for implementation of the Pid_Manager_Thread file and documentation.
+*                Faris was responsible for implementation of the Pid_Manager_Multithread file.
+*/
+//  Implements Runnable to create a threaded program
 public class Pid_Manager_Thread implements Runnable {
-    private int pidThreadID;
-    private int pidThreadSleepTime;
-    private Pid_Manager pidManager;
+    private int pidThreadID;            //  int that represents a unique ID for each thread
+    private int pidThreadSleepTime;     //  int that established how long thread will sleep
+    private Pid_Manager pidManager;     //  Pid_Manager that will facilitate allocation and release of pids
 
+    //  Thread constructor method
+    //  Takes int for pidThreadID, int for pidThreadSleepTime, and Pid_Manager for pidManager as parameters
     Pid_Manager_Thread(int id, int time, Pid_Manager manager) {
         this.pidThreadID = id;
         this.pidThreadSleepTime = time;
@@ -11,24 +22,30 @@ public class Pid_Manager_Thread implements Runnable {
         System.out.println("Creating Thread " + this.pidThreadID);
     }
 
+    //  Required method for implementation of thread 
+    //  Allows thread to run and perform task
     @Override
     public void run() {
 
         System.out.println("Running Thread " + this.pidThreadID);
 
+        //  Pid is allocated from pidManager
         Integer pidID = pidManager.allocate_pid();
 
+        //  If Pid was not successfully allocated, loop until it is.
         while (pidID == -1) {
             System.out.println("All pids currently in use. Attempting to reallocate...");
             pidID = pidManager.allocate_pid();
         }
 
+        //  Put thread to sleep for some duration of time
         try {
             Thread.sleep(pidThreadSleepTime);                                 
         } catch (InterruptedException e) {
             System.out.println("Interrupting Thread " + this.pidThreadID);
         }
 
+        //  Release Pid from thread use
         pidManager.release_pid(pidID);
         System.out.println("Stopping Thread " + this.pidThreadID);
     }
